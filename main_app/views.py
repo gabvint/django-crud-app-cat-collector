0dffccefd9f510277a133d74eedf45bd00baa414
gabtongol <geybtongol@gmail.com>
2024-09-10 22:50:01 -0400

    display indv cat and fixed urls acrooss multiple pages

--- main_app/views.py
+++ main_app/views.py
@@ -3,21 +3,6 @@ from django.http import HttpResponse
 from .models import Cat
 
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Bambi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
 # Create your views here.
 
 def home(request):
@@ -27,5 +12,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
-    cats = Cat.objects.all()
+    cats = Cat.objects.all() # returns a list of all cats
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id) # return a specific cat object
+    return render(request, 'cats/detail.html', {'cat': cat})
