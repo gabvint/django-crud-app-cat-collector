ce47ed621434eb3d4615023c77e13ff495dd384b
Gabrielle Tongol <geybtongol@gmail.com>
2025-05-05 13:33:32 +0800

    custom model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from datetime import date 
 # Create your models here.
 
 MEALS = (
@@ -34,6 +35,9 @@ class Cat(models.Model):
     
     def get_absolute_url(self):
         return reverse('cat-detail', kwargs={'cat_id': self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 
 class Feeding(models.Model):
